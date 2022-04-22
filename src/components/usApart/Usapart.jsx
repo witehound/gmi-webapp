81b09133d48e3a185dcefd0220c644f134a0f726
InventedMost <netvid00@gmail.com>
2022-04-22 02:21:08 +0100

    fixing project in min component  error

--- src/components/usApart/Usapart.jsx
+++ src/components/usApart/Usapart.jsx
@@ -1,11 +1,11 @@
 import UsapartCard from "../UsapartCard/UsapartCard";
 import "./Usapart.css";
-import usApartArray  from '../../arrays/usApartArrayLeft'
-import usApartArrayRight  from '../../arrays/usApartArrayRight'
-import Pmind from "../Pmind/Pmind";
+import "./Pmind.css"
+import usApartArray from "../../arrays/usApartArrayLeft";
+import usApartArrayRight from "../../arrays/usApartArrayRight";
 
 function Usapart() {
-  const add = true
+  const add = true;
   return (
     <div className="usapart-div">
       <div className="usapart-div-inner">
@@ -19,15 +19,26 @@ function Usapart() {
         <div className="usapart-div-bottom">
           <div className="usapart-div-bottom-inner">
             <div className="usapart-div-bottom-left">
-              {usApartArray.map(({img, h2, p }) => (
+              {usApartArray.map(({ img, h2, p }) => (
                 <UsapartCard key={h2} img={img} h2={h2} p={p} />
               ))}
             </div>
             <div className="usapart-div-bottom-right">
-            {usApartArrayRight.map(({img, h2, p }) => (
+              {usApartArrayRight.map(({ img, h2, p }) => (
                 <UsapartCard key={h2} img={img} h2={h2} p={p} />
               ))}
-             <Pmind/>
+              <div className="project-card-div-wrap">
+                <div className="project-card-div-top">
+                  <h1 className="project-card-div-top-h1">
+                    Have a project in mind?
+                  </h1>
+                </div>
+                <div className="project-card-div-bottom">
+                  <button className="project-card-div-top-button">
+                    Build at gmi
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
