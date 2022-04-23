08c73e6e79a5893507c256194d25c616cd40451e
InventedMost <netvid00@gmail.com>
2022-04-23 11:32:10 +0100

    framer motion added to hero, nft stat, crypto stat, usapart

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -3,7 +3,18 @@ import './Copyright.css'
 function Copyright() {
   return (
     <div className='copy-right-div'>
-       © Copyright 2022 Studio GMI. All Rights Reserved
+     <div className="copy-right-div-top">
+     <div className="copy-right-div-conatct">
+       Contact
+     </div>
+     <div className="copy-right-div-at">
+       @Studi Gmi
+     </div>
+     </div>
+     <div className="copy-right-div-bottom">
+        © Copyright 2022 Studio GMI. All Rights Reserved
+     </div>
+      
     </div>
   )
 }
