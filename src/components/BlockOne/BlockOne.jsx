d1e5dca3aa7dd713100fe9ffb8bc79dd75674498
InventedMost <netvid00@gmail.com>
2022-05-04 12:09:44 +0100

    setting up block one

--- src/components/BlockOne/BlockOne.jsx
+++ src/components/BlockOne/BlockOne.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import './BlockOne.css'
-import blockOneA from '../../../assets/blockOneA.jpg'
-import blockOneB from '../../../assets/blockOneB.jpg'
+import React from "react";
+import "./BlockOne.css";
+import blockOneA from "../../../assets/blockOneA.jpg";
+import blockOneB from "../../../assets/blockOneB.jpg";
+import Button from "../Button/Button";
 
 function BlockOne() {
   return (
-    <div className='blockone'>
+    <div className="blockone">
       <div className="blockone-container">
-          <div className="blockone-imgone">
-            <img src={blockOneA} alt="" className='blockonea'/>
+        <div className="blockone-imgone">
+          <img src={blockOneA} alt="" className="blockonea" />
+        </div>
+        <div className="blockone-text">
+          <div className="blockone-text-container">
+            <h1 className="blockone-text-h1">Getting Businesses web3 Ready.</h1>
+            <div className="blockone-text-p-wrap">
+              <p className="blockone-text-p">
+                Studio GMI offers a full 360° suite to its clients. From
+                consultative training, design, build and integration within web3
+                and blockchain technologies to marketing, partnerships and after
+                sales service.Studio GMI offers a full 360° suite to its
+                clients. From consultative training, design, build and
+                integration within web3 and block- chain technologies to
+                marketing, partnerships and after sales service.
+              </p>
+              <p className="blockone-text-p">
+                Let us guide you through the maze that is blockchain
+                technologies to create and strategize an effective way for your
+                business to enter the world of web3
+              </p>
+              <p className="blockone-text-p">
+                Our goal is to help businesses transition into WEB3 and Next
+                generation techno- logies
+              </p>
+            </div>
+            <div className="blockone-text-button">
+              <Button text={"Get Involved"} color={"white"} />
+            </div>
           </div>
-          <div className="blockone-text">
-            blockone
-          </div>
-          <div className="blockone-imgtwo">
-            <img src={blockOneB} alt="" className='blockoneb'/>
-          </div>
+        </div>
+        <div className="blockone-imgtwo">
+          <img src={blockOneB} alt="" className="blockoneb" />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default BlockOne
+export default BlockOne;
