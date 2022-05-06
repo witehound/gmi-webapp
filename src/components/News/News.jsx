95854e26059a588c565d6d0a91ab07be5f5b80ad
InventedMost <netvid00@gmail.com>
2022-05-06 16:50:57 +0100

    updated news images

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,5 +1,9 @@
 import "./News.css";
 import NewsCard from "./NewsCard/NewsCard";
+import newsImgOne from "../../../assets/newsImg1.png";
+import newsImgTwo from "../../../assets/nike.gif"
+import newsImgThree from "../../../assets/starbucks.jpeg"
+import newsImgFour from "../../../assets/selfridges.webp"
 
 function News() {
   return (
@@ -7,14 +11,14 @@ function News() {
       <div className="news-container">
         <div className="news-top">
           <div className="news-top-container">
-            <NewsCard />
-            <NewsCard />
+            <NewsCard newsImg={newsImgOne} title={'Alpha Pony Society'}/>
+            <NewsCard newsImg={newsImgTwo} title={'Nike x RTFKT'}/>
           </div>
         </div>
         <div className="news-top">
           <div className="news-top-container">
-            <NewsCard />
-            <NewsCard />
+            <NewsCard newsImg={newsImgThree} title={'StarBucks'}/>
+            <NewsCard newsImg={newsImgFour} title={'Selfridges'}/>
           </div>
         </div>
       </div>
